6d5275430af65e391fc457d3286bcf40e603bc88
QuinnBast <Quinn.Bast@hotmail.com>
2017-11-05 09:02:16 -0600

    *Some changes to login

--- php/login.php
+++ php/login.php
@@ -1,6 +1,13 @@
 <?
 include 'serverConn.php'
 
+$usr = $_POST('Username')
+$pass = $_POST('Password')
 
-
-?>
+$sql = "SELECT * FROM ITStaff WHERE Username = $usr and Password = $pass"
+if($DB->query($sql) === TRUE)
+{
+	echo "User found";
+} else {
+	echo "No User";
+}
